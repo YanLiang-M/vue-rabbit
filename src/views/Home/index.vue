4d3a8f82b6ba1dd51e3e72a0870748b28ce0ddec
yanruyu <2653977591@qq.com>
2026-01-22 18:49:50 +0800

    home-整体结构区分以及分类的数据渲染

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+import HomeBanner from "./components/HomeBanner.vue";
+import HomeCategory from "./components/HomeCategory.vue";
+import HomeHot from "./components/HomeHot.vue";
+import HomeNew from "./components/HomeNew.vue";
+import HomeProduct from "./components/HomeProduct.vue";
+</script>
+
 <template>
-  <div style="height: 1200px">我是home页面</div>
+  <div class="container">
+    <HomeCategory></HomeCategory>
+    <HomeBanner></HomeBanner>
+  </div>
+  <HomeNew></HomeNew>
+  <HomeHot></HomeHot>
+  <HomeProduct></HomeProduct>
 </template>
